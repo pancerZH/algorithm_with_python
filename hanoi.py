f80f7704bf572fad23856d07b70457c19a2b831b
DESKTOP-DLOBL4I\张文喆 <942740938@qq.com>
2017-11-26 10:18:49 +0800

    精简代码

--- hanoi.py
+++ hanoi.py
@@ -1,8 +1,6 @@
 def move(origin, middle, destination, left_num):
-    if left_num == 0:
-        return
-    move(origin, destination, middle, left_num-1)
-    print('move {} from {} to {}'.format(left_num, origin, destination))
-    move(middle, origin, destination, left_num-1)
-
+    if left_num != 0:
+        move(origin, destination, middle, left_num-1)
+        print('move {} from {} to {}'.format(left_num, origin, destination))
+        move(middle, origin, destination, left_num-1)
 move('A', 'B', 'C', 10)
