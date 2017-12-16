fc1cdaba7c2b8c72c6854209bb10bdcf3f7e3c48
DESKTOP-DLOBL4I\张文喆 <942740938@qq.com>
2017-12-16 20:04:52 +0800

    增加计算最大公共子串功能

--- LCS.py
+++ LCS.py
@@ -1,5 +1,3 @@
-# 这里讨论最长公共子序列问题，非最长公共子串
-
 def max(num1, num2):
     '''返回参数中较大的那一个'''
     if num1 > num2:
@@ -8,8 +6,8 @@ def max(num1, num2):
         return num2
 
 
-def dynamic(str1, str2):
-    '''动态规划方法解决LCS问题'''
+def subsequence(str1, str2):
+    '''动态规划方法解决最长公共子序列问题'''
     # 检查参数类型是否正确
     assert type(str1) == str and type(str2) == str, 'wrong type!'
 
@@ -43,13 +41,55 @@ def dynamic(str1, str2):
     return table[-1][-1], result
 
 
+def substring(str1, str2):
+    '''动态规划方法解决最长公共子串问题'''
+    # 检查参数类型是否正确
+    assert type(str1) == str and type(str2) == str, 'wrong type!'
+
+    # 构建动态规划的表格
+    table = [[0 for i in range(len(str1)+1)] for j in range(len(str2)+1)]
+
+    # 进行动态规划
+    for i in range(1, len(str2)+1):
+        for j in range(1, len(str1)+1):
+            if str2[i-1] == str1[j-1]:
+                table[i][j] = table[i-1][j-1] + 1
+            else:
+                table[i][j] = 0
+    
+    # 找到最长公共子串的结束位置
+    max_i = max_j = max_length = 0
+    for i in range(1, len(str2)+1):
+        for j in range(1, len(str1)+1):
+            if table[i][j] > max_length:
+                max_length = table[i][j]
+                max_i = i
+                max_j = j
+    
+    result = []
+    i = max_i
+    j = max_j
+    while table[i][j] != 0:
+        result.append(str2[i-1])
+        i -= 1
+        j -= 1
+    result.reverse()
+
+    return table[max_i][max_j], result
+
+
 def main():
     str1 = input('Please enter the 1st string: ')
     str2 = input('Please enter the 2nd string: ')
 
-    length, result = dynamic(str1, str2)
-    print('LCS\' length: {}'.format(length))
-    print('LCS: {}'.format(''.join(result)))
+    length, result = subsequence(str1, str2)
+    print('LCS_subsequence\' length: {}'.format(length))
+    print('LCS_subsequence: {}'.format(''.join(result)))
+
+    print()
+    length, result = substring(str1, str2)
+    print('LCS_substring\' length: {}'.format(length))
+    print('LCS_substring: {}'.format(''.join(result)))
 
 if __name__ == '__main__':
     main()
